810db50033dbe69d5a50cce5fde071998c28bb70
Jean Pierre <contatoribeirodossantos@gmail.com>
2025-10-05 21:51:12 -0300

    feat: logger

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "gingorm/router"
+import (
+	"gingorm/config"
+	"gingorm/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+
+	logger = *config.GetLogger("main")
+
+	err := config.Init()
+
+	if err == nil {
+		logger.Errf("CONFIG INITIALIZATION ERROR: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
